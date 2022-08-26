3d7c6f52584e329d421dc80109135e5131e87d63
Art Savin <art.an.savin@gmail.com>
2022-08-26 09:28:59 +0300

    Initial commit

---
+++ app/src/main/java/com/artsavin/wifighter/WifiTileService.kt
@@ -0,0 +1,21 @@
+package com.artsavin.wifighter
+
+import androidx.wear.tiles.RequestBuilders
+import androidx.wear.tiles.ResourceBuilders
+import androidx.wear.tiles.TileBuilders
+import androidx.wear.tiles.TileService
+import com.google.common.util.concurrent.ListenableFuture
+
+
+class WifiTileService: TileService() {
+
+    override fun onTileRequest(requestParams: RequestBuilders.TileRequest): ListenableFuture<TileBuilders.Tile> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onResourcesRequest(requestParams: RequestBuilders.ResourcesRequest): ListenableFuture<ResourceBuilders.Resources> {
+        TODO("Not yet implemented")
+    }
+
+
+}
